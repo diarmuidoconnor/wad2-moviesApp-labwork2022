87d1b16c3f68a4f776c9ac4f35ae41159d4dfa1b
Diarmuid O Connor <doconnor@wit.ie>
2022-09-09 14:14:32 +0100

    Added filtering of movie list.

--- src/components/filterMoviesCard/index.js
+++ src/components/filterMoviesCard/index.js
@@ -37,9 +37,10 @@ export default function FilterMoviesCard(props) {
   }, []);
 
   const handleChange = (e, type, value) => {
-    e.preventDefault();
-    // Completed later
-  };
+    e.preventDefault()
+    props.onUserInput(type, value)   // NEW
+  }
+
   const handleTextChange = (e) => {
     handleChange(e, "name", e.target.value);
   };
