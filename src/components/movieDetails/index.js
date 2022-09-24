f41eaaef95de42ff99fb89e023b4ceca0d9e0316
Diarmuid O Connor <doconnor@wit.ie>
2022-09-24 17:28:49 +0100

    Show movie review excerpts; Refactor Details page.

--- src/components/movieDetails/index.js
+++ src/components/movieDetails/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Chip from "@mui/material/Chip";
 import Paper from "@mui/material/Paper";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
@@ -7,20 +7,22 @@ import StarRate from "@mui/icons-material/StarRate";
 import NavigationIcon from "@mui/icons-material/Navigation";
 import Fab from "@mui/material/Fab";
 import Typography from "@mui/material/Typography";
-
+import Drawer from "@mui/material/Drawer";
+import MovieReviews from "../movieReviews";
 
 const root = {
-    display: "flex",
-    justifyContent: "center",
-    flexWrap: "wrap",
-    listStyle: "none",
-    padding: 1.5,
-    margin: 0,
+  display: "flex",
+  justifyContent: "center",
+  flexWrap: "wrap",
+  listStyle: "none",
+  padding: 1.5,
+  margin: 0,
 };
 const chip = { margin: 0.5 };
 
-const MovieDetails = ( props) => {
-  const movie = props.movie
+const MovieDetails = (props) => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const movie = props.movie;
 
   return (
     <>
@@ -32,10 +34,7 @@ const MovieDetails = ( props) => {
         {movie.overview}
       </Typography>
 
-      <Paper 
-        component="ul" 
-        sx={root}
-      >
+      <Paper component="ul" sx={root}>
         <li>
           <Chip label="Genres" sx={chip} color="primary" />
         </li>
@@ -57,10 +56,7 @@ const MovieDetails = ( props) => {
         />
         <Chip label={`Released: ${movie.release_date}`} />
       </Paper>
-      <Paper 
-        component="ul" 
-        sx={root}
-      >
+      <Paper component="ul" sx={root}>
         <li>
           <Chip label="Production Countries" sx={chip} color="primary" />
         </li>
@@ -73,16 +69,24 @@ const MovieDetails = ( props) => {
       <Fab
         color="secondary"
         variant="extended"
+        onClick={() => setDrawerOpen(true)}
         sx={{
-            position: "fixed",
-            bottom: 2,
-            right: 2
+          position: "fixed",
+          bottom: "1em",
+          right: "1em",
         }}
       >
         <NavigationIcon />
         Reviews
       </Fab>
-      </>
+      <Drawer
+        anchor="top"
+        open={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
+      >
+        <MovieReviews movie={movie} />
+      </Drawer>
+    </>
   );
 };
-export default MovieDetails ;
+export default MovieDetails;
