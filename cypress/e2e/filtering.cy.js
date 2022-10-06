375b3f485734912d65d604d7642ef237fd37dd5b
Diarmuid O Connor <doconnor@wit.ie>
2022-10-06 11:59:14 +0100

    Started E2E testing - Base and filtering

---
+++ cypress/e2e/filtering.cy.js
@@ -0,0 +1,78 @@
+let movies; // List of movies from TMDB
+
+// Utility functions
+const filterByTitle = (movieList, string) =>
+  movieList.filter((m) => m.title.toLowerCase().search(string) !== -1);
+
+const filterByGenre = (movieList, genreId) =>
+  movieList.filter((m) => m.genre_ids.includes(genreId));
+
+describe("Home Page", () => {
+  before(() => {
+    // Get movies from TMDB and store in movies variable.
+    cy.request(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
+        "TMDB_KEY"
+      )}&language=en-US&include_adult=false&include_video=false&page=1`
+    )
+      .its("body") // Take the body of HTTP response from TMDB
+      .then((response) => {
+        movies = response.results;
+      });
+  });
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  describe("Filtering", () => {
+    describe("By movie title", () => {
+      it("should only display movies with m in the title", () => {
+        let searchString = "m";
+        let matchingMovies = filterByTitle(movies, searchString);
+        cy.get("#filled-search").clear().type(searchString); // Enter m in text box
+        cy.get(".MuiCardHeader-content").should(
+          "have.length",
+          matchingMovies.length
+        );
+        cy.get(".MuiCardHeader-content").each(($card, index) => {
+          cy.wrap($card).find("p").contains(matchingMovies[index].title);
+        });
+      });
+      it("should display no movies when the search string has no matches", () => {
+        // Do a second test for certainty!
+        let searchString = "xyxxzyyzz";
+        cy.get("#filled-search").clear().type(searchString); // Enter m in text box
+        cy.get(".MuiCardHeader-content").should("have.length", 0);
+      });
+      it("should only display movies with o in the title", () => {
+        // Do a second normal test for certainty!
+        let searchString = "o";
+        let matchingMovies = filterByTitle(movies, searchString);
+        cy.get("#filled-search").clear().type(searchString); // Enter m in text box
+        cy.get(".MuiCardHeader-content").should(
+          "have.length",
+          matchingMovies.length
+        );
+        cy.get(".MuiCardHeader-content").each(($card, index) => {
+          cy.wrap($card).find("p").contains(matchingMovies[index].title);
+        });
+      });
+    });
+    describe("By movie genre", () => {
+      it("should display movies with the specified genre only", () => {
+        const selectedGenreId = 35;
+        const selectedGenreText = "Comedy";
+        const matchingMovies = filterByGenre(movies, selectedGenreId);
+        cy.get("#genre-select").click();
+        cy.get("li").contains(selectedGenreText).click();
+        cy.get(".MuiCardHeader-content").should(
+          "have.length",
+          matchingMovies.length
+        );
+        cy.get(".MuiCardHeader-content").each(($card, index) => {
+          cy.wrap($card).find("p").contains(matchingMovies[index].title);
+        });
+      });
+    });
+  });
+});
