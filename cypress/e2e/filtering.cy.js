af898af1ed72208468ed3ef364351b9734c165ed
Diarmuid O Connor <doconnor@wit.ie>
2022-10-17 10:13:53 +0100

    E2E tests - Added Navigation tests and tweaked other specs

--- cypress/e2e/filtering.cy.js
+++ cypress/e2e/filtering.cy.js
@@ -1,4 +1,4 @@
-import { filterByGenre, filterByTitle } from   '../support/e2e'  
+import { filterByGenre, filterByTitle } from "../support/e2e";
 
 let movies; // List of Discover movies from TMDB
 
@@ -19,39 +19,42 @@ describe("Filtering", () => {
     cy.visit("/");
   });
 
-    describe("By movie title", () => {
-      it("only display movies with 'm' in the title", () => {
-        const searchString = "m";
-        const matchingMovies = filterByTitle(movies, searchString);
-        cy.get("#filled-search").clear().type(searchString); // Enter m in text box
-        cy.get(".MuiCardHeader-content").should(
-          "have.length",
-          matchingMovies.length
-        );
-        cy.get(".MuiCardHeader-content").each(($card, index) => {
-          cy.wrap($card).find("p").contains(matchingMovies[index].title);
-        });
-      });
-      it("handles case when there are no matches", () => {
-        const searchString = "xyxxzyyzz";
-        cy.get("#filled-search").clear().type(searchString); // Enter m in text box
-        cy.get(".MuiCardHeader-content").should("have.length", 0);
+  describe("By movie title", () => {
+    it("only display movies with 'm' in the title", () => {
+      const searchString = "m";
+      const matchingMovies = filterByTitle(movies, searchString);
+      cy.get("#filled-search").clear().type(searchString); // Enter m in text box
+      cy.get(".MuiCardHeader-content").should(
+        "have.length",
+        matchingMovies.length
+      );
+      cy.get(".MuiCardHeader-content").each(($card, index) => {
+        cy.wrap($card).find("p").contains(matchingMovies[index].title);
       });
     });
-    describe("By movie genre", () => {
-      it("display movies with the selected genre", () => {
-        const selectedGenreId = 35;
-        const selectedGenreText = "Comedy";
-        const matchingMovies = filterByGenre(movies, selectedGenreId);
-        cy.get("#genre-select").click();
-        cy.get("li").contains(selectedGenreText).click();
-        cy.get(".MuiCardHeader-content").should(
-          "have.length",
-          matchingMovies.length
-        );
-        cy.get(".MuiCardHeader-content").each(($card, index) => {
-          cy.wrap($card).find("p").contains(matchingMovies[index].title);
-        });
+    it("handles case when there are no matches", () => {
+      const searchString = "xyxxzyyzz";
+      cy.get("#filled-search").clear().type(searchString); // Enter m in text box
+      cy.get(".MuiCardHeader-content").should("have.length", 0);
+    });
+  });
+  describe("By movie genre", () => {
+    it("show movies with the selected genre", () => {
+      const selectedGenreId = 35;
+      const selectedGenreText = "Comedy";
+      const matchingMovies = filterByGenre(movies, selectedGenreId);
+      cy.get("#genre-select").click();
+      cy.get("li").contains(selectedGenreText).click();
+      cy.get(".MuiCardHeader-content").should(
+        "have.length",
+        matchingMovies.length
+      );
+      cy.get(".MuiCardHeader-content").each(($card, index) => {
+        cy.wrap($card).find("p").contains(matchingMovies[index].title);
       });
     });
   });
+  describe("Combined genre and title", () => {
+    // Exercise
+  });
+});
